f65423bf6ff273731baa062e15b3ed5025696b06
Overgrown <overgrownmc@gmail.com>
2025-09-18 12:39:25 -0400

    Reverted back to old version since apparently that is the right one that is fixed

--- src/main/java/dev/overgrown/sync/Sync.java
+++ src/main/java/dev/overgrown/sync/Sync.java
@@ -1,9 +1,17 @@
 package dev.overgrown.sync;
 
+import dev.overgrown.sync.factory.power.type.ActionOnDeathPower;
 import dev.overgrown.sync.factory.registry.SyncTypeRegistry;
+import dev.overgrown.sync.networking.ModPackets;
+import dev.overgrown.sync.utils.KeyPressManager;
+import io.github.apace100.apoli.component.PowerHolderComponent;
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import io.github.apace100.apoli.util.NamespaceAlias;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,13 +28,38 @@ public class Sync implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        SyncTypeRegistry.register();
-
         HAS_ASPECTSLIB = FabricLoader.getInstance().isModLoaded("aspectslib");
         if (HAS_ASPECTSLIB) {
             LOGGER.info("AspectsLib detected - compatibility enabled");
         }
 
         NamespaceAlias.addAlias("apoli", MOD_ID);
+        SyncTypeRegistry.register();
+
+        // Register death event handler
+        ServerLivingEntityEvents.AFTER_DEATH.register((entity, damageSource) -> {
+            if (entity instanceof ServerPlayerEntity) {
+                PowerHolderComponent component = PowerHolderComponent.KEY.get(entity);
+                component.getPowers(ActionOnDeathPower.class).forEach(power -> {
+                    power.onDeath(damageSource, entity.getMaxHealth()); // Use max health as damage amount
+                });
+            }
+        });
+
+        ServerPlayNetworking.registerGlobalReceiver(
+                ModPackets.KEY_PRESS_UPDATE,
+                (server, player, handler, buf, responseSender) -> {
+                    String key = buf.readString();
+                    boolean pressed = buf.readBoolean();
+                    server.execute(() ->
+                            KeyPressManager.updateKeyState(player.getUuid(), key, pressed)
+                    );
+                }
+        );
+
+        ServerPlayConnectionEvents.DISCONNECT.register(
+                (handler, server) ->
+                        KeyPressManager.removePlayer(handler.player.getUuid())
+        );
     }
 }
--- src/main/java/dev/overgrown/sync/SyncServer.java
+++ src/main/java/dev/overgrown/sync/SyncServer.java
@@ -1,45 +1,12 @@
 package dev.overgrown.sync;
 
-import dev.overgrown.sync.factory.power.type.ActionOnDeathPower;
-import dev.overgrown.sync.networking.ModPackets;
 import dev.overgrown.sync.utils.KeyPressManager;
-import io.github.apace100.apoli.component.PowerHolderComponent;
 import net.fabricmc.api.DedicatedServerModInitializer;
-import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.minecraft.server.network.ServerPlayerEntity;
 
 public class SyncServer implements DedicatedServerModInitializer {
     @Override
     public void onInitializeServer() {
         ServerTickEvents.END_SERVER_TICK.register(KeyPressManager::serverTick);
-
-        // Register death event handler
-        ServerLivingEntityEvents.AFTER_DEATH.register((entity, damageSource) -> {
-            if (entity instanceof ServerPlayerEntity) {
-                PowerHolderComponent component = PowerHolderComponent.KEY.get(entity);
-                component.getPowers(ActionOnDeathPower.class).forEach(power -> {
-                    power.onDeath(damageSource, entity.getMaxHealth()); // Use max health as damage amount
-                });
-            }
-        });
-
-        ServerPlayNetworking.registerGlobalReceiver(
-                ModPackets.KEY_PRESS_UPDATE,
-                (server, player, handler, buf, responseSender) -> {
-                    String key = buf.readString();
-                    boolean pressed = buf.readBoolean();
-                    server.execute(() ->
-                            KeyPressManager.updateKeyState(player.getUuid(), key, pressed)
-                    );
-                }
-        );
-
-        ServerPlayConnectionEvents.DISCONNECT.register(
-                (handler, server) ->
-                        KeyPressManager.removePlayer(handler.player.getUuid())
-        );
     }
 }
