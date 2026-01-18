4d5ce28f2de1a96e477c941d8f2cc097ade9c116
Lucas Bandermann <lucas@threetag.net>
2026-01-18 18:20:03 +0100

    Fixed entity_texture_overlay power to override player skin now

--- src/main/java/dev/overgrown/sync/utils/RenderingUtils.java
+++ src/main/java/dev/overgrown/sync/utils/RenderingUtils.java
@@ -17,10 +17,10 @@ public class RenderingUtils {
         return PowerHolderComponent.getPowers(entity, EntityTextureOverlayPower.class);
     }
 
-    public static Identifier getPrimaryOverlayTexture(LivingEntity entity) {
+    public static Identifier getPrimaryOverlayTexture(LivingEntity entity, boolean slimModel, boolean firstPerson) {
         List<EntityTextureOverlayPower> powers = getTextureOverlays(entity);
-        if (!powers.isEmpty() && powers.get(0).isActive()) {
-            return powers.get(0).getTextureLocation();
+        if (!powers.isEmpty() && powers.get(0).isActive() && (powers.get(0).shouldShowFirstPerson() || !firstPerson)) {
+            return slimModel ? powers.get(0).getSlimTextureLocation() : powers.get(0).getWideTextureLocation();
         }
         return null;
     }
