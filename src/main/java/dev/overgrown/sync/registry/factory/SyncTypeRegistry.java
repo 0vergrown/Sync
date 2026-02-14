42edd1e18b5f88679b3860b869f4417dd0334efc
Overgrown <overgrownmc@gmail.com>
2026-02-14 01:09:02 -0500

    Added `prevent_label_render` power type

--- src/main/java/dev/overgrown/sync/registry/factory/SyncTypeRegistry.java
+++ src/main/java/dev/overgrown/sync/registry/factory/SyncTypeRegistry.java
@@ -44,6 +44,7 @@ import dev.overgrown.sync.factory.power.type.modify_enchantment_level.ModifyEnch
 import dev.overgrown.sync.factory.power.type.modify_model_parts.ModifyModelPartsPower;
 import dev.overgrown.sync.factory.power.type.modify_player_model.ModifyPlayerModelPower;
 import dev.overgrown.sync.factory.power.type.pose.PosePower;
+import dev.overgrown.sync.factory.power.type.prevent_label_render.PreventLabelRenderPower;
 import dev.overgrown.sync.factory.power.type.prevent_sprinting_particles.PreventSprintingParticlesPower;
 import dev.overgrown.sync.factory.power.type.sprinting.SprintingPower;
 import dev.overgrown.sync.registry.factory.utils.ApoliRegistryHelper;
@@ -66,6 +67,7 @@ public class SyncTypeRegistry {
         ApoliRegistryHelper.registerPowerFactory(ModifyModelPartsPower.getFactory());
         ApoliRegistryHelper.registerPowerFactory(ModifyPlayerModelPower.getFactory());
         ApoliRegistryHelper.registerPowerFactory(PosePower.getFactory());
+        ApoliRegistryHelper.registerPowerFactory(PreventLabelRenderPower.getFactory());
         ApoliRegistryHelper.registerPowerFactory(PreventSprintingParticlesPower.getFactory());
         ApoliRegistryHelper.registerPowerFactory(SprintingPower.getFactory());
 
